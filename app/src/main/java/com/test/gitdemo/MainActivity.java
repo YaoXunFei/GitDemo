d5d731c912e4d87a6f5cdf07e74a871eeb669333
YaoXunFei <1124840334@qq.com>
2021-11-29 14:29:41 +0800

    test commit2

--- app/src/main/java/com/test/gitdemo/MainActivity.java
+++ app/src/main/java/com/test/gitdemo/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("test");
     }
 }
